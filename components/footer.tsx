4d8befa3c7be6cfc71bee28033088bc6be279b34
vaqeh <34025faqih@gmail.com>
2023-06-15 20:48:17 +0700

    movie

---
+++ components/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 w-full bg-gray-200 text-center py-2 text-gray-600 text-sm">
+      <div className="container mx-auto">
+        <p className="mb-1">© 2023 My Website. All rights reserved.</p>
+        <p className="mb-0">Watermark</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
