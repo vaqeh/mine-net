e222b21b32c76f3972fe9571300fc68dfad21dc9
vaqeh <34025faqih@gmail.com>
2023-06-15 21:49:24 +0700

    ls

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className=" bottom-0 w-full bg-black text-white text-center py-2 text-sm font-serif">
+    <footer className=" bottom-0 w-full bg-black text-white text-center py-2 text-sm">
       <div className="container mx-auto">
         <p className="mb-1">© 2023 My Website. Modivied.</p>
         <p className="mb-0">VAQEH</p>
