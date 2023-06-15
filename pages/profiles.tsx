e957f14b09d3828823b085b2f25ea35c05061495
vaqeh <34025faqih@gmail.com>
2023-06-15 20:56:11 +0700

    yuhu

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -12,9 +12,9 @@ const images = [
   "/images/smile.png",
 ];
 
-// interface UserCardProps {
-//   name: string;
-// }
+interface UserCardProps {
+  name: string;
+}
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -33,25 +33,25 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 
-// const UserCard: React.FC<UserCardProps> = ({ name }) => {
-//   const imgSrc = images[Math.floor(Math.random() * 4)];
+const UserCard: React.FC<UserCardProps> = ({ name }) => {
+  const imgSrc = images[Math.floor(Math.random() * 4)];
 
-//   return (
-//     <div className="group flex-row w-44 mx-auto">
-//       <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
-//         <img
-//           draggable={false}
-//           className="w-max h-max object-contain"
-//           src={imgSrc}
-//           alt=""
-//         />
-//       </div>
-//       <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">
-//         {name}
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className="group flex-row w-44 mx-auto">
+      <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
+        <img
+          draggable={false}
+          className="w-max h-max object-contain"
+          src={imgSrc}
+          alt=""
+        />
+      </div>
+      <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">
+        {name}
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
   const router = useRouter();
@@ -65,15 +65,13 @@ const App = () => {
     <div className="flex items-center h-full justify-center">
       <div className="flex flex-col">
         <h1 className="text-3xl md:text-6xl text-white text-center">
-          Who's watching?
+          Who&#39;s watching?
         </h1>
         <div className="flex items-center justify-center gap-8 mt-10">
-          {/* <div onClick={() => selectProfile()}>
+          <div onClick={() => selectProfile()}>
             <UserCard name={currentUser?.name} />
-          </div> */}
-          <img src="/images/love.png" alt="p" className=" h-10 w-auto" />
+          </div>
         </div>
-        <div className=" text-white"> name : {currentUser?.name}</div>
       </div>
     </div>
   );
